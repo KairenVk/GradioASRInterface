408a4789ea75487109d992c223cd5c0b2597abb7
KairenVk <kamilsurdacki01@gmail.com>
2024-02-22 15:42:51 +0100

    Refactor, fix encoding issues

--- autocorrect_functions.py
+++ autocorrect_functions.py
@@ -1,8 +1,8 @@
 from autocorrect import Speller
+from app_settings import settings_yaml
 
 
 def autocorrect_text(text, language):
-    from init import settings_yaml
     autocorrect_languages = settings_yaml['autocorrect_languages']
     spell = Speller(autocorrect_languages[language])
     return spell(text)
